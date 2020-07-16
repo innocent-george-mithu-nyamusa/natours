771a06d2b976ba81111686c1ec864f315a939ee3
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-07-17 01:16:45 +0200

    fixed await error

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,18 +1,19 @@
 /* eslint-disable*/
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_51GqmuKDxfQhvhY0EGbD8BTuHQjqrhx0D728kaRP6HtxUVAA7TizuZRn1EQufsgbCmcemSSBSuWINIuUcIIKXUjw400yDEQ2Ndp');
+const stripe = Stripe(
+  'pk_test_51GqmuKDxfQhvhY0EGbD8BTuHQjqrhx0D728kaRP6HtxUVAA7TizuZRn1EQufsgbCmcemSSBSuWINIuUcIIKXUjw400yDEQ2Ndp'
+);
 
-export const bookTour = tourId => {
-    try{
-        // 1.) Get checkout session from API
-        const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`)
-        // Create Checkout form + change credit card
-        await stripe.redirectToCheckout({
-            sessionId: session.data.session.id
-        })
-    }catch(err){
-        showAlert('error', err)
-    }
-
-}
+export const bookTour = async tourId => {
+  try {
+    // 1.) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // Create Checkout form + change credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    showAlert('error', err);
+  }
+};
